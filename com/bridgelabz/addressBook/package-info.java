93e921a844168a3ce4619eaf09adeaecd90fafdd
Sanjana K R <sanjanakr97@gmail.com>
2020-07-16 01:07:37 +0530

    Changed Author Name and added compilation command and execution command in package-info file

--- com/bridgelabz/addressBook/package-info.java
+++ com/bridgelabz/addressBook/package-info.java
@@ -2,7 +2,8 @@
  * 
  */
 /**
- * @author user
- *
+ * @author Sanjana K R
+ * Compilation Command: javac -d destination_address_package_folder AddressBook.java
+ * Execution Command: java package_name AddressBook
  */
 package com.bridgelabz.addressBook;
